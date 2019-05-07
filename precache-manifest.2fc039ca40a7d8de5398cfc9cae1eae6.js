1d00028a697be3018c1d5a3f59b449f86e35b66d
AlAnrin <almia.anrin@gmail.com>
2019-05-07 14:57:40 +0300

    Updates

---
+++ precache-manifest.2fc039ca40a7d8de5398cfc9cae1eae6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c82cae8344a2552ef6e",
+    "url": "/weather-test-react/static/js/runtime~main.b6a0358d.js"
+  },
+  {
+    "revision": "41db3894cff845a2a89d",
+    "url": "/weather-test-react/static/js/main.310d4107.chunk.js"
+  },
+  {
+    "revision": "376ba06285acd70d505f",
+    "url": "/weather-test-react/static/js/2.a7b161cf.chunk.js"
+  },
+  {
+    "revision": "41db3894cff845a2a89d",
+    "url": "/weather-test-react/static/css/main.2e5dc937.chunk.css"
+  },
+  {
+    "revision": "673d67a708ee5054593deffddbb607f4",
+    "url": "/weather-test-react/index.html"
+  }
+];
